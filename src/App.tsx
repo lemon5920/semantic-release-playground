5428e140da730e12cff047ec26db9622d7e3b72f
lemon5920 <lemon5920@gmail.com>
2022-01-01 23:41:28 +0800

    feat: Add test env variable.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>{import.meta.env.VITE_TEST} Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
