7e810a66a992761c458c1633f62d5a1eb2972a35
JhanYing <lemon5920@gmail.com>
2022-01-02 22:01:22 +0800

    fix: Base url for static site. (#7)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "semantic-release-playground", // https://vitejs.dev/guide/static-deploy.html#github-pages
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
